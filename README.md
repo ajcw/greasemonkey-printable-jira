0b1a8b37867e22eef7fbd6c1b85a9238d30dd8de
John Catterfeld <john@ajcw.com>
2012-12-10 12:03:11 +0000

    Removed unnecessary CSS file, updated readme and finalised script

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 greasemonkey-printable-jira
 ===========================
 
-Greasemonkey script for formatting Jira tickets into a small, printable format
+Greasemonkey script for formatting Jira tickets into a small, printable format.
+
+How to use (Firefox):
+
+1. Install the Greasemonkey add-on to you Firefox browser
+2. With Firefox visit the RAW greasemonkey script in this repository (https://github.com/ajcw/greasemonkey-printable-jira/raw/master/greasemonkey-printable-jira.user.js) - and you'll be automatically prompted to install the script
+3. Click install
+
+The next time you visit an individual Jira ticket it will reformat the page to a printable card.  Disable the Greasmonkey script to revert back to the previous format.
--- greasemonkey-printable-jira.user.js
+++ greasemonkey-printable-jira.user.js
@@ -3,51 +3,67 @@
 // @description    Formats the Jira tickets to be small printable boxes. Script is based on 'myCss' by Mike Cao
 // @include        https://*.atlassian.net/browse/*
 // @namespace      http://www.ajcw.com/
-// @grant       	none
+// @grant           none
 // ==/UserScript==
 
 var css = new Array();
 
 function writeStyle(css) {
-    var style = document.createElement('style');
-    style.type = 'text/css';
+    // Pull in the styles (defined at the bottom) into a print stylesheet
+    var printStyles = document.createElement('style');
+    printStyles.type = 'text/css';
+    printStyles.setAttribute("media", "print");
     if (document.getElementsByTagName) {
-        document.getElementsByTagName('head')[0].appendChild(style);
-        if (style.sheet && style.sheet.insertRule) {
+        document.getElementsByTagName('head')[0].appendChild(printStyles);
+        if (printStyles.sheet && printStyles.sheet.insertRule) {
             for (var i = 0; i < css.length; i++) {
-                style.sheet.insertRule(css[i], 0);
+                printStyles.sheet.insertRule(css[i], 0);
             }
         }
     }
+
+    // Also pull in the styles normally so we can preview them in the browser (before we print) - I'm sure we could refactor this repetition
+    var previewStylesInBrowser = document.createElement('style');
+    previewStylesInBrowser.type = 'text/css';
+    if (document.getElementsByTagName) {
+        document.getElementsByTagName('head')[0].appendChild(previewStylesInBrowser);
+        if (previewStylesInBrowser.sheet && previewStylesInBrowser.sheet.insertRule) {
+            for (var i = 0; i < css.length; i++) {
+                previewStylesInBrowser.sheet.insertRule(css[i], 0);
+            }
+        }
+    }
+
 }
 
 function addStyle(style) {
     css[css.length] = style;
 }
 
-// Define your CSS here
+// Hide content
+addStyle(".aui-header,.command-bar,.navigator-issue-only #stalker .page-navigation, header .breadcrumbs li, #heading-avatar, #footer, #activitymodule, #greenhopper-agile-issue-web-panel, #datesmodule, #descriptionmodule, #addcomment, #issuedetails, #details-module_heading, #peoplemodule_heading, #votes-val, #watchers-val, .twixi {display:none !important;}");
 
-// Hide elements
-addStyle(".aui-header { display: none; }");
-addStyle(".command-bar, .navigator-issue-only #stalker .page-navigation {display:none;}");
-addStyle("#footer, #activitymodule, #greenhopper-agile-issue-web-panel, #datesmodule, #descriptionmodule, #addcomment, #issuedetails, #details-module_heading, #peoplemodule_heading, #votes-val, #watchers-val {display:none;}");
-addStyle("header .breadcrumbs li {display: none !important;}");
-
-// Style content
-addStyle(".breadcrumbs {display: inline !important;margin: 0 !important;float:right;}");
-addStyle("header .breadcrumbs li + li {display: block !important;background:none;}");
-addStyle("#key-val {font-size:56px;}");
-addStyle("#heading-avatar {float:right !important;margin: 12px 24px !important;}");
-addStyle("h1 {margin:0 0 0 30px !important;padding:0 !important;position:absolute !important;top:24px;width:50%;background:none !important;}");
-addStyle("header {position:relative;}");
+// Layout styles
+addStyle("body, #stalker {min-width: 0 !important;}");
+addStyle(".aui-theme-default #content {padding:10px;width:50%;overflow:hidden;border:2px solid #000 !important;border-radius:6px}");
+addStyle(".page-type-navigator #content > .content-container {width: 100% !important;font-size:11px;border:0;}");
 addStyle("#viewissuesidebar {float:left;clear:left;width:100%;padding:0;}");
 addStyle(".issue-main-column {float:left;width:100%;}");
-addStyle(".issue-main-column {float:left;width:100%;}");
-addStyle(".people-details {border-top: 1px solid #DDDDDD;}");
+
+// Heading styles
+addStyle("header {position:relative;}");
+addStyle(".breadcrumbs {display: inline !important;margin: 0 10px 0 0 !important;float:left;font-size:36px !important;}");
+addStyle("header .breadcrumbs li + li {display: block !important;background:none;}");
+addStyle("h1 {margin:0 !important;padding:0 !important;width:60%;background:none !important;font-size:20px;float:left;}");
+
+// Content styles
+addStyle(".people-details {border-top: 1px solid #DDD;}");
 addStyle("ul.item-details dl dt {width: 9.5em;}");
 addStyle("#viewissuesidebar ul.item-details dl dd {text-align:left;}");
-addStyle("section > .content-container {width:600px;}");
-addStyle("body, #stalker {min-width: 0 !important;}");
+addStyle(".module > .mod-header + .mod-content, .property-list .wrap {padding:0;}");
+addStyle(".navigator-issue-only #viewissuesidebar .item-details dd > span, .property-list .name {margin:0;}");
+addStyle(".type-textarea {clear:left;float:left;}");
+addStyle(".twixi-wrap {padding:0;}");
 
 // Writes CSS to the document
 writeStyle(css);
