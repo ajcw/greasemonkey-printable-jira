16339711a0382b143f6c65ce598a30fcd807c6cf
John Catterfeld <john@ajcw.com>
2012-12-10 17:08:49 +0000

    Tweaked header layout to accomodate potentially longer titles

--- greasemonkey-printable-jira.user.js
+++ greasemonkey-printable-jira.user.js
@@ -41,29 +41,31 @@ function addStyle(style) {
 }
 
 // Hide content
-addStyle(".aui-header,.command-bar,.navigator-issue-only #stalker .page-navigation, header .breadcrumbs li, #heading-avatar, #footer, #activitymodule, #greenhopper-agile-issue-web-panel, #datesmodule, #descriptionmodule, #addcomment, #issuedetails, #details-module_heading, #peoplemodule_heading, #votes-val, #watchers-val, .twixi, #attachmentmodule, #linkingmodule {display:none !important;}");
+addStyle(".aui-header,.command-bar,.navigator-issue-only #stalker .page-navigation, header .breadcrumbs li, #heading-avatar, #footer, #activitymodule, #greenhopper-agile-issue-web-panel, #datesmodule, #descriptionmodule, #addcomment, #issuedetails, #details-module_heading, #peoplemodule_heading, #votes-val, #watchers-val, .twixi, #attachmentmodule, #linkingmodule, #timetrackingmodule {display:none !important;}");
 
 // Layout styles
-addStyle("body, #stalker {min-width: 0 !important;}");
-addStyle(".aui-theme-default #content {padding:10px;width:50%;overflow:hidden;border:2px solid #000 !important;border-radius:6px}");
-addStyle(".page-type-navigator #content > .content-container {width: 100% !important;font-size:11px;border:0;}");
+addStyle("body, #stalker {min-width:0!important;}");
+addStyle(".aui-theme-default #content {padding:10px;width:50%;overflow:hidden;border:2px solid #000!important;border-radius:6px;background:#FFF;}");
+addStyle(".page-type-navigator #content > .content-container {width:100%!important;font-size:11px;border:0;}");
 addStyle("#viewissuesidebar {float:left;clear:left;width:100%;padding:0;}");
+addStyle(".issue-header-content {padding:0!important;}");
 addStyle(".issue-main-column {float:left;width:100%;}");
 
 // Heading styles
 addStyle("header {position:relative;}");
-addStyle(".breadcrumbs {display: inline !important;margin: 0 10px 0 0 !important;float:left;font-size:36px !important;}");
-addStyle("header .breadcrumbs li + li {display: block !important;background:none;}");
-addStyle("h1 {margin:0 !important;padding:0 !important;width:60%;background:none !important;font-size:20px;float:left;}");
+addStyle(".breadcrumbs {font-size:36px!important;float:left;margin:0!important;}");
+addStyle("header .breadcrumbs li + li {display:block!important;background:none;}");
+addStyle("h1 {margin:0 0 0 110px!important;padding:0!important;background:none!important;font-size:20px;}");
 
 // Content styles
-addStyle(".people-details {border-top: 1px solid #DDD;}");
-addStyle("ul.item-details dl dt {width: 9.5em;}");
+addStyle(".people-details {border-top:1px solid #DDD;}");
+addStyle("ul.item-details dl dt {width:9.5em;}");
 addStyle("#viewissuesidebar ul.item-details dl dd {text-align:left;}");
 addStyle(".module > .mod-header + .mod-content, .property-list .wrap {padding:0;}");
 addStyle(".navigator-issue-only #viewissuesidebar .item-details dd > span, .property-list .name {margin:0;}");
 addStyle(".type-textarea {clear:left;float:left;}");
 addStyle(".twixi-wrap {padding:0;}");
+addStyle("ul.breadcrumbs > li, .content-body  {padding:0!important;}");
 
 // Writes CSS to the document
 writeStyle(css);
